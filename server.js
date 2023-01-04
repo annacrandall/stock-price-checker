475194bb72432fb9f39311e40d73f43e2e5b8a79
ana crandall <107437409+annacrandall@users.noreply.github.com>
2023-01-04 05:43:21 +0000

    Set up content security request and mongoDB.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
-
+const helmet = require('helmet'); 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
@@ -16,6 +16,13 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    scriptSrc: ["'self'"],
+    styleSrc: ["'self'"]
+  }
+}));
 
 //Index page (static HTML)
 app.route('/')
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,27 @@ const server = require('../server');
 const mongoose = require('mongoose'); 
 
 chai.use(chaiHttp);
+mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true});
+// conntect to mongoDB
+const Schema = mongoose.Schema;
+const stockSchema = new Schema({
+  symbol: {type: String, required: true}, 
+  likes: {type: [String], default: []}
+}); 
+// create Schema 
+const model = mongoose.model('Stock', stockSchema);
+//create model 
 
-suite('Functional Tests', function() {
-});
+//tests to put inside suite 
+// Viewing one stock: GET request to /api/stock-prices/
+// Viewing one stock and liking it: GET request to /api/stock-prices/
+// Viewing the same stock and liking it again: GET request to /api/stock-prices/
+// Viewing two stocks: GET request to /api/stock-prices/
+// Viewing two stocks and liking them: GET request to /api/stock-prices/
+
+
+suite('Functional Tests', async function() {
+})
+
+
+
