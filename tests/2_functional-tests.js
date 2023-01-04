2b7a2f06d51d455fbe8d8cbc4485ae8a892977a8
ana crandall <107437409+annacrandall@users.noreply.github.com>
2023-01-04 04:18:39 +0000

    Set up project for development.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,9 +2,9 @@ const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
+const mongoose = require('mongoose'); 
 
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
 });
